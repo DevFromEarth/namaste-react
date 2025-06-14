c2d1764c9e82f2e8a151103d969be7d86998cee9
Utkarsh <PRAS432118@exactsoftware.com>
2025-06-14 16:40:44 +0530

    consume : class

--- Episode-11 Data is the new Oil/src/component/About.js
+++ Episode-11 Data is the new Oil/src/component/About.js
@@ -1,10 +1,14 @@
 import React from "react";
 import UserClass from "./UserClass";
+import UserContext from "../utils/UserContext";
 
 class About extends React.Component {
   render() {
     return (
       <div className="about">
+        <UserContext.Consumer>
+          {({ loggedInUser }) => <h1>{loggedInUser}</h1>}
+        </UserContext.Consumer>
         <h1>About</h1>
         <h1>This is namaste React Web Series</h1>
         <UserClass name="1st" />
